d49d9860e3c1336feb6e7cd739306406a2a607e4
Lucas Popp <lmp122@case.edu>
2020-02-05 15:23:09 +0100

    Added router & CORS headers

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 // Initializes the server
 const express = require('express');
 const bodyParser = require('body-parser');
+const router = require('./router');
 
 const app = express();
 
+// Add some CORS headers to the response
+app.use((_, res, next) => {
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        'Access-Control-Allow-Headers': ['Authorization', 'Accept', 'Content-Type']
+    });
+    
+    next();
+})
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+router.tagAuthedRoutes(app);
+router.setRoutes(app);
 
 module.exports = app;
---
+++ router.js
@@ -0,0 +1,44 @@
+const _ = require('lodash');
+const controllers = require('./controllers');
+
+const ROUTE_MAP = {
+    // '/ROUTE': {
+    //     'METHOD': {
+    //         requiresAuth: false,
+    //         handler: undefined
+    //     }
+    // }
+}
+
+function tagAuthedRoutes(app) {
+    for (const route in ROUTE_MAP) {
+        app.use(route, (req, res, next) => {
+            if (req.requiresAuth == undefined && !!_.get(ROUTE_MAP, [route, req.method.toLowerCase()])) {
+                req.requiresAuth = ROUTE_MAP[route][req.method.toLowerCase()].requiresAuth;
+            }
+
+            next();
+        })
+    }
+}
+
+function setRoutes(app) {
+    for (const route in ROUTE_MAP) {
+        for (const method in ROUTE_MAP[route]) {
+            app[method](route, async (req, res) => {
+                try {
+                    await ROUTE_MAP[route][method].handler(req, res);
+                } catch (err) {
+                    console.error(err.stack);
+                    res.status(500).end(err.message);
+                }
+            });
+        }
+    }
+}
+
+module.exports = {
+    ROUTE_MAP,
+    tagAuthedRoutes,
+    setRoutes
+}
